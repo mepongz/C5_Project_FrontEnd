1cc23621c6fcbd5f6893605ed86cc01f969f450a
System Administrator <root@Meffreys-MacBook-Pro.local>
2022-04-08 17:34:07 +0800

    UPDATED

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey! Welcome to C5 Project
         </a>
       </header>
     </div>
